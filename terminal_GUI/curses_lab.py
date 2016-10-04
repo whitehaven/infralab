db85e136fc4eaf61a3a8ea66cfed442bec895c92
Alex Crist <accrist@gmail.com>
2016-10-04 01:23:54 -0600

    added start of curses demo

--- terminal_GUI/curses_lab.py
+++ terminal_GUI/curses_lab.py
@@ -18,6 +18,8 @@ diagram = ('  ||     ||  ',
            '      |      '
            )
 
+# TODO:  add temperature sensors that have random positions
+
 for line_number, line_contents in enumerate(diagram):
     stdscr.addstr(line_number, 2, line_contents)
 
